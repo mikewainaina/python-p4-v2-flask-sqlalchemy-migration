e89330fce314239fc19c8027afe181443a8561ef
mikewainaina <michael.wainaina@student.moringaschool.com>
2024-06-27 21:13:56 +0300

    init

--- server/models.py
+++ server/models.py
@@ -20,7 +20,6 @@ class Employee(db.Model):
     def __repr__(self):
         return f'<Employee {self.id}, {self.name}, {self.salary}>'
 
-
 class Department(db.Model):
     __tablename__ = 'departments'
 
